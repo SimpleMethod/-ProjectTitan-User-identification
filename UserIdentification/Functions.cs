65bb27486c3d16756e76c68c1b2042612e154969
Michał <s085218@student.tu.kielce.pl>
2017-06-18 16:44:42 +0200

    Dodanie plików

---
+++ UserIdentification/Functions.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Text;
+using System.Management;
+using System.Security.Cryptography;
+using System.Net.Sockets;
+using System.Net;
+
+namespace UserIdentification
+{
+
+    class Functions
+    {
+        private static string IP = "127.0.0.1";
+        private static int Port = 9997;
+
+        private static string HashData(string Data)
+        {
+
+            byte[] Hash;
+            using (MD5 Md5 = MD5.Create())
+            {
+                Md5.Initialize();
+                Md5.ComputeHash(Encoding.ASCII.GetBytes(Data));
+                Hash = Md5.Hash;
+            }
+            Data = Convert.ToBase64String(Hash).ToString();
+            return Data.Substring(0, Math.Min(Data.Length, 6));
+
+        }
+        private static int RandomNumbers()
+        {
+            Random Random = new Random();
+            int Result = 0;
+            for (int i = 0; i < 4; ++i)
+            {
+                Result = Result + Random.Next(0, 9999);
+            }
+            return Result;
+        }
+        public static string MachineID()
+        {
+            string Data = "";
+            try
+            {
+
+                ManagementObjectSearcher Cpu = new ManagementObjectSearcher("Select * From Win32_processor");
+                ManagementObjectCollection Cpulist = Cpu.Get();
+
+                foreach (ManagementObject CpuL in Cpulist)
+                {
+                    Data += (string)CpuL["ProcessorID"];
+                }
+                ManagementObjectSearcher Base = new ManagementObjectSearcher("SELECT * FROM Win32_BaseBoard");
+                ManagementObjectCollection BaseBordList = Base.Get();
+                foreach (ManagementObject BaseL in BaseBordList)
+                {
+                    Data += (string)BaseL["SerialNumber"];
+                }
+            }
+            catch (ManagementException e)
+            {
+                Data = RandomNumbers().ToString();
+            }
+            return HashData(Data);
+        }
+        public static void Server(string Data)
+        {
+            try
+            {
+                IPAddress AdressIP = IPAddress.Parse(IP);
+                TcpListener TCPConnect = new TcpListener(AdressIP, Port);
+                TCPConnect.Start();
+                Socket TCPSocket = TCPConnect.AcceptSocket();
+                byte[] b = new byte[100]; int k = TCPSocket.Receive(b);
+                ASCIIEncoding SafeASCII = new ASCIIEncoding();
+                TCPSocket.Send(SafeASCII.GetBytes(Data));
+                TCPSocket.Close();
+                TCPConnect.Stop();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Błąd:" + e.StackTrace);
+            }
+
+        }
+    }
+}
---
+++ UserIdentification/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace UserIdentification
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            Console.Write(@"         
+               .,-:;//;:=,
+          . :H@@@MM@M#H/.,+%;,
+       ,/X+ +M@@M@MM%=,-%HMMM@X/,
+     -+@MM;$M@@MH+,;XMMMM@MMMM@+
+    ;@M@@M- XM@X;.-+XXXXXHHH@M@M#@/.
+  ,%MM@@MH,@%=            .-=-=:=,.
+  =@#@@@MX.,               -%HX$$%%%:;
+ =-./@M@M$                  .;@MMMM@MM:
+ X@/-$MM/                    . +MM@@@M$
+,@M@H::@:                   . =X#@@@@-
+,@@@MMX,.                    /H- ;@M@M=
+.H@@@@M@+,                   %MM+..%#$.
+ /MMMM@MMH/.                 XM@MH;=;
+  /%+%$XHH@$=             , .H@@@@MX,
+   .=------.           -%H.,@@@@@MX,
+   .%MM@@@HHHXX$$$%+-.:$MMX =M@@MM%.
+     =XMMM@MM@MM#H;,-+HMM@M+/MMMX=
+       =%@M@M#@$-.=$@MM@@@M;%M%=
+         ,:+$+-,/H#MMMMMMM@==,
+               =++%%%%+/:-.﻿");
+            Functions.Server(Functions.MachineID());
+        }
+    }
+}
